3057a490307e1af7e7ecac3098c9ca66b2a13e43
Kristian Rosales <r.kristian40@gmail.com>
2023-03-21 15:24:55 -0500

    met mvp

--- api/server.js
+++ api/server.js
@@ -9,22 +9,41 @@ server.get("/", (req, res) => {
   res.send("hello world");
 });
 
+// server.post("/api/users/", async (req, res) => {
+//   const { name, bio } = req.body;
+//   if (!name || !bio) {
+//     return res
+//       .status(400)
+//       .json({ message: "Please provide name and bio for the user" });
+//   } else {
+//     try {
+//       const newlyCreatedUser = await user.create({ name, bio });
+//       return res.status(201).json(newlyCreatedUser);
+//     } catch (error) {
+//       console.error(error.message);
+//       return res
+//         .status(500)
+//         .json({ message: "The users information could not be retrieved" });
+//     }
+//   }
+// });
+
 server.post("/api/users", async (req, res) => {
   const { name, bio } = req.body;
   if (!name || !bio) {
     return res
       .status(400)
       .json({ message: "Please provide name and bio for the user" });
-  }
-  try {
-    const newUser = req.body;
-    const user = await user.insert(newUser);
-    return res.status(201).json(newUser);
-  } catch (err) {
-    console.error(err.message);
-    return res.status(500).json({
-      message: "There was an error while saving the user to the database",
-    });
+  } else {
+    try {
+      const newUser = await user.insert({ name, bio });
+      return res.status(201).json(newUser);
+    } catch (error) {
+      console.error(error.message);
+      return res.status(500).json({
+        message: "There was an error while saving the user to the database",
+      });
+    }
   }
 });
 
@@ -42,19 +61,68 @@ server.get("/api/users", async (req, res) => {
 
 server.get("/api/users/:id", async (req, res) => {
   try {
-    const usher = await user.find();
-    const id = usher.findById;
-    if (!id) {
+    const id = req.params.id;
+    const User = await user.findById(id);
+    if (!User) {
       return res
         .status(404)
         .json({ message: "The user with the specified ID does not exist" });
     }
-    res.json(user);
+    res.json(User);
   } catch (error) {
     console.error(error.message);
     return res
       .status(500)
       .json({ message: "The user information could not be retrieved" });
+  }
+});
+
+server.delete("/api/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const userToDelete = await user.findById(id);
+    if (!userToDelete) {
+      return res
+        .status(404)
+        .json({ message: "The user with the specified ID does not exist" });
+    }
+    const deletedUser = await user.remove(id);
+    if (!deletedUser) {
+      return res.status(500).json({ message: "The user could not be removed" });
+    }
+    res.json(deletedUser);
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
+server.put("/api/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const { bio, name } = req.body;
+    const User = await user.findById(id);
+    if (!User) {
+      return res
+        .status(404)
+        .json({ message: "The user with the specified ID does not exist" });
+    }
+
+    if (!bio || !name) {
+      return res
+        .status(400)
+        .json({ message: "Please provide name and bio for the user" });
+    }
+
+    const updatedUser = await user.update(id, { name, bio });
+
+    if (!updatedUser) {
+      return res
+        .status(500)
+        .json({ message: "The user information could not be modified" });
+    }
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    console.error(error.message);
   }
 });
 
